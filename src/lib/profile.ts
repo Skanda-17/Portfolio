55fa6633f7ba8dd130084f49f2a833c9a8a52cf3
Skanda-Bharadwaj-GM <23aiml057@bnmit.in>
2026-05-02 14:18:46 +0530

    Update resume, GitHub profile, and projects

--- src/lib/profile.ts
+++ src/lib/profile.ts
@@ -3,6 +3,6 @@ export const profile = {
   name: 'Skanda Bharadwaj GM',
   linkedin: 'https://linkedin.com/in/skanda-bharadwaj-gm-718971358/',
   instagram: 'https://instagram.com/skanda._17?igsh=MWQyM3h3aWFvaHJyOA==',
-  github: 'https://github.com/Skanda-Bharadwaj-GM',
+  github: 'https://github.com/Skanda-17',
   phone: '+91 8217289336', // Replace with your number
 }
